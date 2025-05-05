a5565527a0a43997809b6e83ecef1e54fed29a02
thalles <thallessantos513@gmail.com>
2025-05-05 09:05:15 -0300

    ultimamodificação

--- Conta Click/app/src/main/java/com/ifsc/contaclicks/Planeta.java
+++ Conta Click/app/src/main/java/com/ifsc/contaclicks/Planeta.java
@@ -1,6 +1,7 @@
 package com.ifsc.contaclicks;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,11 @@ public class Planeta extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+            Bundle b = getIntent().getExtras();
+            b.get("nome");
+            TextView tv = findViewById(R.id.textView);
+            tv.setText(b.get("nome").toString());
         });
     }
 }
