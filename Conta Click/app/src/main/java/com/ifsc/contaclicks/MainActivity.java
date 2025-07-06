fe9a2228576888c0b572557ccbac4677a622b5d3
thalless44 <thallessantos513@gmail.com>
2025-07-06 01:40:06 -0300

    sensores

--- Conta Click/app/src/main/java/com/ifsc/contaclicks/MainActivity.java
+++ Conta Click/app/src/main/java/com/ifsc/contaclicks/MainActivity.java
@@ -1,42 +1,46 @@
 package com.ifsc.contaclicks;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Integer i = 0;
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    int i=0;
+    SensorManager mSensorManager;
+    Sensor sensor;
+    TextView tx,ty,tz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tx =findViewById(R.id.textViewX);
+        ty =findViewById(R.id.textViewY);
+        tz =findViewById(R.id.textViewZ);
 
-        //Associando objeto informando a variavel local
-        TextView tv = findViewById(R.id.TextView);
-        tv.setText(getString(R.string.app_name));
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        Button b = findViewById(R.id.button);
+//        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        b.setOnClickListener(new View.OnClickListener() {
+        mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-            @Override
-            public void onClick(View v) {
-
-                tv.setText(Integer.toString(i));
-                i++;
-
-            }
-        });
-
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        tx.setText("X:"+Float.toString(sensorEvent.values[0]));
+        ty.setText("Y:"+Float.toString(sensorEvent.values[1]));
+        tz.setText("Z:"+Float.toString(sensorEvent.values[2]));
 
     }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
